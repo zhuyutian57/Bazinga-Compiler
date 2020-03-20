1e1f287aaa1e80b80a9e1a1335f3d615db03ed02
朱雨田 <zhuyutian57@gmail.com>
2020-03-20 20:55:16 +0800

    chang Items to Itemset

--- parser/Itemset.h
+++ parser/Itemset.h
@@ -13,50 +13,54 @@ namespace parser {
 #define Lok int
 #define Core first
 #define Lookahead second
+#define PRODUCT_ID Core.first
+#define DOT Core.second
 
-class Items {
+class Itemset {
 
 public:
-  Items(){}
-  ~Items(){}
-  bool operator==(const Items& it) const {
+  Itemset() { cores = new std::set<Kernel>; }
+  ~Itemset() {}
+  bool operator==(const Itemset& it) const {
     return items == it.items;
   }
-  bool operator<(const Items& it) const {
+  bool operator<(const Itemset& it) const {
     return items < it.items;
   }
-
-  const int& Number() { return number; }
-  const int Size() { return items.size(); }
 
   void Set_number(const int num) {
     number = num;
   }
+
   void Add_item(const Item& it) {
     items.insert(it);
-    cores.insert(it.Core);
+    cores->insert(it.Core);
   }
-  const std::set<Item>& Item_set() {
+
+  auto Erase(auto it) { return items.erase(it); }
+
+  const int& Number() { return number; }
+  const int Size() { return items.size(); }
+  const std::set<Item>& Items() {
     return items;
   }
-  const std::set<Kernel>& Cores() {
+  const std::set<Kernel>* Cores() {
     return cores;
   }
   
   struct Core_cmp {
-    bool operator()(
-        const Items& items1,
-        const Items& items2) {
-      return items1.cores < items2.cores;
+    bool operator()(const Itemset& it1,
+        const Itemset& it2) {
+      return (*it1.cores) < (*it2.cores);
     }
   };
 
 private:
   int number;
   std::set<Item> items;
-  std::set<Kernel> cores;
+  std::set<Kernel>* cores;
 
-}; // class Item
+}; // class Itemset
 
 } // namespace parser
 
