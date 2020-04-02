a12d036fe3fe6586c4d61dd5bc73566baab7ec19
朱雨田 <zhuyutian57@gmail.com>
2020-04-02 16:03:39 +0800

    rename typename T to ValType

--- bin/Stack.h
+++ bin/Stack.h
@@ -6,29 +6,29 @@
 
 namespace bin {
 
-template<typename T>
+template<typename ValType>
 class Stack {
 
 public:
   Stack() { v.clear(); }
   ~Stack() {}
   //TODO add exception
-  T operator[](const int& index) const {
+  ValType operator[](const int& index) const {
     return v[v.size() + index];
   }
 
   bool Empty() const { return v.empty(); }
   
-  void Push(const T& e) { v.push_back(e); }
+  void Push(const ValType& e) { v.push_back(e); }
   
   void Pop() { v.pop_back(); }
 
   std::size_t Size() const { return v.size(); }
 
-  T& Top() { return v.back(); }
+  ValType& Top() { return v.back(); }
 
 private:
-  std::vector<T> v;
+  std::vector<ValType> v;
 
 }; // class Stack
 
